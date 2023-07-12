5f86ff17e1d7a343f5bf7afd7018f433f77d48a0
Joe Galante <jgalante@ecr.ai>
2023-07-12 14:52:45 -0400

    Added full delete functionality

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Page from './Page'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import Axios from 'axios'
 import LoadingSpinner from './LoadingSpinner'
 import ReactMarkdown from 'react-markdown'
 import { Tooltip } from 'react-tooltip'
 import NotFound from './NotFound'
+import StateContext from '../StateContext'
+import DispatchContext from '../DispatchContext'
 
 function ViewSinglePost() {
+	const navigate = useNavigate()
+	const appDispatch = useContext(DispatchContext)
+	const appState = useContext(StateContext)
 	const { id } = useParams()
 	const [isLoading, setIsLoading] = useState(true)
 	const [post, setPost] = useState([])
@@ -39,34 +44,61 @@ function ViewSinglePost() {
 	}
 
 	const dateFormatted = new Date(post.createdDate).toLocaleDateString()
+	function isOwner() {
+		return appState.loggedIn && appState.user.username === post.author.username
+	}
+
+	async function deleteHandler() {
+		const areYouSure = window.confirm('Do you really want to delete this post?')
+
+		if (areYouSure) {
+			try {
+				const response = await Axios.delete(`/post/${id}`, {
+					data: { token: appState.user.token },
+				})
+				if (response.data === 'Success') {
+					appDispatch({
+						type: 'flashMessage',
+						value: 'Post was successfully deleted.',
+					})
+					navigate(`/profile/${appState.user.username}`)
+				}
+			} catch (error) {
+				console.error('There was an error', error)
+			}
+		}
+	}
 
 	return (
 		<Page title={post.title}>
 			<div className="d-flex justify-content-between">
 				<h2>{post.title}</h2>
-				<span className="pt-2">
-					<Link
-						to={`/post/${post._id}/edit`}
-						data-tooltip-content="Edit"
-						data-tooltip-id="edit"
-						className="text-primary mr-2">
-						<i className="fas fa-edit"></i>
-					</Link>
-					<Tooltip
-						id="edit"
-						class="custom-tooltip"
-					/>{' '}
-					<a
-						data-tooltip-content="Delete"
-						data-tooltip-id="delete"
-						className="delete-post-button text-danger">
-						<i className="fas fa-trash"></i>
-					</a>
-					<Tooltip
-						id="delete"
-						class="custom-tooltip"
-					/>
-				</span>
+				{isOwner() && (
+					<span className="pt-2">
+						<Link
+							to={`/post/${post._id}/edit`}
+							data-tooltip-content="Edit"
+							data-tooltip-id="edit"
+							className="text-primary mr-2">
+							<i className="fas fa-edit"></i>
+						</Link>
+						<Tooltip
+							id="edit"
+							class="custom-tooltip"
+						/>{' '}
+						<a
+							data-tooltip-content="Delete"
+							data-tooltip-id="delete"
+							className="delete-post-button text-danger"
+							onClick={deleteHandler}>
+							<i className="fas fa-trash"></i>
+						</a>
+						<Tooltip
+							id="delete"
+							class="custom-tooltip"
+						/>
+					</span>
+				)}
 			</div>
 
 			<p className="text-muted small mb-4">
